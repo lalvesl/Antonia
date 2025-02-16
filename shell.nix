3c53f140d50b61a88738529d03ae609f7545229c
Lucas Alves <alvesdelima.lucas45@gmail.com>
2025-02-16 16:00:25 -0300

    feat: update sin calculated

--- shell.nix
+++ shell.nix
@@ -12,7 +12,10 @@ pkgs.mkShell {
     arduino-cli
     avrdude
     clang-tools
+    nodejs
   ];
 
-  shellHook = '''';
+  shellHook = ''
+    node -e "console.log(Array.from({length: 1024}, (_,i)=>i*(Math.PI/(2*1024))).map(i=>Math.sin(i)).map(i=>i*2*1024).map(i=>Math.round(i)).map(i=>i.toString(16)).map(h=>h.padStart(4, '0')).map(h=>'0x'+h).join(', '))" > sin.txt
+  '';
 }
