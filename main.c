fddd0ddcba94be0a3406cec4c0334310198ee4ea
Lucas Alves <alvesdelima.lucas45@gmail.com>
2025-02-16 16:37:15 -0300

    feat: implement 12 bit DAC

--- main.c
+++ main.c
@@ -27,9 +27,15 @@
 #define DAC8 PORTB0  // bit 8 do DAC (PB0)
 #define DAC9 PORTB1  // bit 9 do DAC (PB1)
 
+#define DAC7_0 PORTL // 8 bits menos significativos do DAC (PORTD)
+#define DAC8 PORTC0  // bit 8 do DAC (PB0)
+#define DAC9 PORTC1  // bit 9 do DAC (PB1)
+#define DAC12 PORTC2 // bit 10 do DAC (PB2)
+#define DAC11 PORTC3 // bit 11 do DAC (PB3)
+
 // ============================================================================
 // --- Protótipo das Funções ---
-void DAC10(uint16_t dac_val); // função para enviar os dados para o DAC
+void DAC12(uint16_t dac_val); // função para enviar os dados para o DAC
 void sinewave();              // função para construir a senoide de 10 bits
 void rampwave();              // função para gerar o sinal em rampa
 
@@ -40,17 +46,17 @@ extern uint16_t sine12bits[1024];
 // ============================================================================
 // --- Função Principal ---
 int main() {
-  PORTC |= (1 << FUNC); // habilita pull-up interno
-  DDRB |=
-      (1 << DAC8) |
-      (1 << DAC9); // configura saída para os 2 bits mais significativos do DAC
-  DDRD = 0xFF;   // configura saída para os 8 bits menos significativos do DAC
+  PORTB |= (1 << FUNC); // habilita pull-up interno
+  DDRC |=
+      (1 << DAC8) | (1 << DAC9) | (1 << DAC12) |
+      (1 << DAC11); // configura saída para os 2 bits mais significativos do DAC
+  DDRL = 0xFF;   // configura saída para os 8 bits menos significativos do DAC
   DAC7_0 = 0x00; // inicializa 8 bits menos significativos do DAC em LOW
-  PORTB &= ~(1 << DAC8) &
-           ~(1 << DAC9); // inicializa 2 bits mais significativos do DAC em LOW
+  PORTC &= ~(1 << DAC8) & ~(1 << DAC9) & ~(1 << DAC12) &
+           ~(1 << DAC11); // inicializa 2 bits mais significativos do DAC em LOW
 
   while (1) {
-    if (PINC & (1 << FUNC)) // se entrada de função em HIGH
+    if (PINB & (1 << FUNC)) // se entrada de função em HIGH
       sinewave();           // gera sinal senoidal
     else                    // senão
       rampwave();           // gera rampa crescente
@@ -66,43 +72,52 @@ int main() {
 // --- Desenvolvimento das Funções ---
 
 // ============================================================================
-// --- DAC10 ---
+// --- DAC12 ---
 // Envia o valor binário de 10 bits para o DAC
-void DAC10(uint16_t dac_val) {
+void DAC12(uint16_t dac_val) {
 
   dac_val &= 0x03FF; // limite de 10 bits
 
   DAC7_0 = dac_val; // atualiza os 8 bits menos significativos
 
   if (dac_val >> 8 & 0x0001) // atualiza o bit 8
-    PORTB |= (1 << DAC8);
+    PORTC |= (1 << DAC8);
   else
-    PORTB &= ~(1 << DAC8);
+    PORTC &= ~(1 << DAC8);
 
   if (dac_val >> 9 & 0x0001) // atualiza o bit 9
-    PORTB |= (1 << DAC9);
+    PORTC |= (1 << DAC9);
   else
-    PORTB &= ~(1 << DAC9);
+    PORTC &= ~(1 << DAC9);
 
-} // end DAC10
+  if (dac_val >> 10 & 0x0001) // atualiza o bit 8
+    PORTC |= (1 << DAC12);
+  else
+    PORTC &= ~(1 << DAC12);
+
+  if (dac_val >> 11 & 0x0001) // atualiza o bit 8
+    PORTC |= (1 << DAC11);
+  else
+    PORTC &= ~(1 << DAC11);
+} // end DAC12
 
 // ============================================================================
 // --- sinewave ---
 // Gera a senoide de 10 bits
 void sinewave() {
-  static uint8_t values = 0x00, // índice do vetor
-      cycle = 1,                // 1 indica ciclo positivo, 0 negativo
-      dir = 1;                  // 1 indica sentido crescente, 0 decrescente
+  static uint16_t values = 0x0000, // índice do vetor
+      cycle = 1,                   // 1 indica ciclo positivo, 0 negativo
+      dir = 1;                     // 1 indica sentido crescente, 0 decrescente
 
   if (cycle) // processa semiciclo positivo
   {
-    DAC10(sine10bits[values]); // valores originais do vetor
+    DAC12(2048 + sine12bits[values]); // valores originais do vetor
 
     if (dir) // sentido crescente
     {
-      values++;          // varre vetor no sentido crescente
-      if (values == 255) // ao atingir o máximo...
-        dir = 0;         //...comuta para sentido decrescente
+      values++;           // varre vetor no sentido crescente
+      if (values == 1023) // ao atingir o máximo...
+        dir = 0;          //...comuta para sentido decrescente
 
     } // end if dir
 
@@ -122,13 +137,13 @@ void sinewave() {
 
   else // processa semiciclo negativo
   {
-    DAC10(1023 - sine10bits[values]); // valores do vetor, subtraídos de 1023
+    DAC12(2048 - sine12bits[values]); // valores do vetor, subtraídos de 2048
 
     if (dir) // sentido crescente
     {
-      values++;          // varre vetor no sentido crescente
-      if (values == 255) // ao atingir o máximo...
-        dir = 0;         //...comuta para sentido decrescente
+      values++;           // varre vetor no sentido crescente
+      if (values == 1023) // ao atingir o máximo...
+        dir = 0;          //...comuta para sentido decrescente
 
     } // end if dir
 
@@ -154,7 +169,7 @@ void sinewave() {
 void rampwave() {
   static uint16_t level = 0x0000; // armazena o nível de tensão
 
-  DAC10(level++); // incrementa level e escreve no DAC
+  DAC12(level++); // incrementa level e escreve no DAC
 }
 
 /* ============================================================================
