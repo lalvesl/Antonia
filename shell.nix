ad56941c3c16dcd6209c1f9f6dcb67583c3b3dd0
Lucas Alves <alvesdelima.lucas45@gmail.com>
2025-02-16 16:06:08 -0300

    feat: update for 2047 max DAC value

--- shell.nix
+++ shell.nix
@@ -16,6 +16,6 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    node -e "console.log(Array.from({length: 1024}, (_,i)=>i*(Math.PI/(2*1024))).map(i=>Math.sin(i)).map(i=>i*2*1024).map(i=>Math.round(i)).map(i=>i.toString(16)).map(h=>h.padStart(4, '0')).map(h=>'0x'+h).join(', '))" > sin.txt
+    node -e "console.log(Array.from({length: 1024}, (_,i)=>i*(Math.PI/(2*1024))).map(i=>Math.sin(i)).map(i=>i*2047).map(i=>Math.round(i)).map(i=>i.toString(16)).map(h=>h.padStart(4, '0')).map(h=>'0x'+h).join(', '))" > sin.txt
   '';
 }
